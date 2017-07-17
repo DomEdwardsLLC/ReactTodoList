7a2593aa0ecc2b763173e1825126033f32a3a9b2
Dom Edwards <dom97@vt.edu>
2017-07-17 19:05:41 -0400

    reverted to the react tutorial todo list

--- src/App.js
+++ src/App.js
@@ -2,48 +2,53 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class TodoList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      todos: Array(3).fill('Randy you got this'),
-    };
-  }
+class TodoList extends React.Component {
   render() {
     return (
-      <div>
-        <input/>
-        <ul>
-          {
-            this.state.todos.map((todo) =>
-              <li>
-                <div>
-                  <label>
-                    {todo}
-                  </label>
-                </div>
-              </li> 
-          )}
-        </ul>
-      </div>
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     );
   }
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {items: [], text: ''};
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <TodoList className="todo" />
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} value={this.state.text} />
+          <button>{'Add #' + (this.state.items.length + 1)}</button>
+        </form>
       </div>
     );
+  }
+
+  handleChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    var newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState((prevState) => ({
+      items: prevState.items.concat(newItem),
+      text: ''
+    }));
   }
 }
 
