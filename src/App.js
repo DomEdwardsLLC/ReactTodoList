2f528a2ebe4f3ebf23a8d40d7e02e15a0f141ef7
Dom Edwards <dom97@vt.edu>
2017-07-18 18:42:53 -0400

    added remove functionality

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ class TodoList extends React.Component {
     return (
       <ul>
         {this.props.items.map(item => (
-          <li key={item.id}>{item.text}</li>
+          <div>
+            <li key={item.id}>{item.text}
+              <button onClick={() => this.props.remove(item.id)}>
+              X
+              </button>
+            </li> 
+          </div>
         ))}
       </ul>
     );
@@ -19,6 +25,7 @@ class App extends Component {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.removeItem = this.removeItem.bind(this);
     this.state = {items: [], text: ''};
   }
 
@@ -26,7 +33,7 @@ class App extends Component {
     return (
       <div>
         <h3>TODO</h3>
-        <TodoList items={this.state.items} />
+        <TodoList items={this.state.items} remove={this.removeItem}/>
         <form onSubmit={this.handleSubmit}>
           <input onChange={this.handleChange} value={this.state.text} />
           <button>{'Add #' + (this.state.items.length + 1)}</button>
@@ -48,6 +55,12 @@ class App extends Component {
     this.setState((prevState) => ({
       items: prevState.items.concat(newItem),
       text: ''
+    }));
+  }
+
+  removeItem(id) {
+    this.setState((prevState) => ({
+      items: prevState.items.filter(item => item.id !== id)
     }));
   }
 }
